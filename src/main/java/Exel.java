0285d86fabe06fa4688ad59e42833d5443e5461f
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2023-11-30 16:47:56 +0900

    뭔 컨벤션이야
    일단 퇴근 전에 급하게 리팩토링해서 실행 해봄. 문제는 좀 이상한 값이 나옴

--- src/main/java/Exel.java
+++ src/main/java/Exel.java
@@ -15,7 +15,8 @@ import java.util.List;
 public class Exel {
 
     private double MONEY = 10_000;
-    static String filePath = "C:\\Users\\Hojun\\Desktop\\simulation\\";
+    private int rowSize=0;
+    static String filePath = "C:\\Users\\dev\\Desktop\\simulation\\";
 
     public void makeExelFile(String fileName) throws IOException, RuntimeErrorException {
         FileInputStream fis = null;
@@ -38,88 +39,85 @@ public class Exel {
         }
     }
 
-    public double readExelFile(FileInputStream fis) {
-        try {
-            HSSFWorkbook workbook = new HSSFWorkbook(fis);
-            int sheetNum = workbook.getNumberOfSheets();
-            String buyingCoin = "";
-            double buyingPrice = 0;
-            for (int i = 0; i < sheetNum; i++) {
-                HSSFSheet sheet;
-                if (buyingCoin.equals("")) { // 만약 산 코인이 없다면 sheet를 계속 돔
-                    sheet = workbook.getSheetAt(i);
-                } else { // 산 코인이 존재하면 해당 코인만 감시
-                    i--; // 코인을 팔때까지 계속 반복문 돌아야함.
-                    sheet = workbook.getSheet(buyingCoin);
-                }
-
-                int currentRow = 1;
-                int rows = sheet.getPhysicalNumberOfRows(); // 해당 시트의 행의 개수
-                List<Double> priceList = new ArrayList<>();
-                for (int rowIndex = currentRow; rowIndex < currentRow + 10; rowIndex++) {
-                    if (currentRow > rows) { // 현재 행이 실제 행보다 크면 빠져나옴
-                        break;
-                    }
-
-                    HSSFRow row = sheet.getRow(rowIndex); // 각 행을 읽어온다
-                    if (row == null) { // 현재 행에 아무 데이터도 없으면 빠져나옴.
-                        break;
-                    }
-
-                    HSSFCell cell = row.getCell(6); // 종가만 읽어온다.
-                    if (cell == null) {
-                        continue;
-                    }
-                    // 읽어온 종가를 priceList 에 넣음
-                    priceList.add(cell.getNumericCellValue());
-                    currentRow++;
-                }
-
-                List<Double> percentList = new ArrayList<>();
-                // 종가 리스트를 이용해서 각 분마다 상승률 퍼센트를 구한다.
-                for (int j = 0; j < priceList.size() - 1; j++) {
-
-
-                    double diff = priceList.get(j) - priceList.get(j + 1);
-                    double percent = Math.round(diff / priceList.get(j + 1) * 10000) / 100.0;
-
-
-                    percentList.add(percent);
-                }
-
-                // 구매조건
-                for(int j=0; j<percentList.size()-1; j++){
-                    if (buyingCoin.equals("") && priceList.get(0) > 150 && percentList.get(j) >1.5 && percentList.get(j+1) > 1.5) {
-                        System.out.println(sheet.getSheetName());
-                        buyingCoin = sheet.getSheetName();
-                        buyingPrice = priceList.get(0);
-                    }
-                }
-
-
-                // 판매조건
-                if (!buyingCoin.equals("")) {
-                    for(int j =0; j<priceList.size(); j++){
-                        double nowPrice = priceList.get(j);
-                        if (nowPrice > buyingPrice * 1.015) {
-                            MONEY = MONEY * 1.015;
-                            buyingPrice = 0;
-                            System.out.println(buyingCoin);
-                            buyingCoin = "";
-                        }
-                        if (nowPrice > buyingPrice * 0.98) {
-                            MONEY = MONEY * 0.98;
-                            buyingPrice = 0;
-                            buyingCoin = "";
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return MONEY;
-    }
+//    public double readExelFile(FileInputStream fis) {
+//        try {
+//            HSSFWorkbook workbook = new HSSFWorkbook(fis);
+//            int sheetNum = workbook.getNumberOfSheets();
+//            String buyingCoin = "";
+//            double buyingPrice = 0;
+//            for (int i = 0; i < sheetNum; i++) {
+//                HSSFSheet sheet;
+//                if (buyingCoin.equals("")) { // 만약 산 코인이 없다면 sheet를 계속 돔
+//                    sheet = workbook.getSheetAt(i);
+//                } else { // 산 코인이 존재하면 해당 코인만 감시
+//                    i--; // 코인을 팔때까지 계속 반복문 돌아야함.
+//                    sheet = workbook.getSheet(buyingCoin);
+//                }
+//
+//                int currentRow = 1;
+//                int rows = sheet.getPhysicalNumberOfRows(); // 해당 시트의 행의 개수
+//                List<Double> priceList = new ArrayList<>();
+//                for (int rowIndex = currentRow; rowIndex < currentRow + 10; rowIndex++) {
+//                    if (currentRow > rows) { // 현재 행이 실제 행보다 크면 빠져나옴
+//                        break;
+//                    }
+//
+//                    HSSFRow row = sheet.getRow(rowIndex); // 각 행을 읽어온다
+//                    if (row == null) { // 현재 행에 아무 데이터도 없으면 빠져나옴.
+//                        break;
+//                    }
+//
+//                    HSSFCell cell = row.getCell(6); // 종가만 읽어온다.
+//                    if (cell == null) {
+//                        continue;
+//                    }
+//                    // 읽어온 종가를 priceList 에 넣음
+//                    priceList.add(cell.getNumericCellValue());
+//                    currentRow++;
+//                }
+//
+//                List<Double> percentList = new ArrayList<>();
+//                // 종가 리스트를 이용해서 각 분마다 상승률 퍼센트를 구한다.
+//                for (int j = 0; j < priceList.size() - 1; j++) {
+//
+//                    double diff = priceList.get(j) - priceList.get(j + 1);
+//                    double percent = Math.round(diff / priceList.get(j + 1) * 10000) / 100.0;
+//                    percentList.add(percent);
+//                }
+//
+//                // 구매조건
+//                for (int j = 0; j < percentList.size() - 1; j++) {
+//                    if (buyingCoin.equals("") && priceList.get(0) > 1.50 && percentList.get(j) > 1.5 && percentList.get(j + 1) > 1.5) {
+//                        System.out.println(sheet.getSheetName());
+//                        buyingCoin = sheet.getSheetName();
+//                        buyingPrice = priceList.get(0);
+//                    }
+//                }
+//
+//
+//                // 판매조건
+//                if (!buyingCoin.equals("")) {
+//                    for (int j = 0; j < priceList.size(); j++) {
+//                        double nowPrice = priceList.get(j);
+//                        if (nowPrice > buyingPrice * 1.015) {
+//                            MONEY = MONEY * 1.015;
+//                            buyingPrice = 0;
+//                            System.out.println(buyingCoin);
+//                            buyingCoin = "";
+//                        }
+//                        if (nowPrice > buyingPrice * 0.98) {
+//                            MONEY = MONEY * 0.98;
+//                            buyingPrice = 0;
+//                            buyingCoin = "";
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return MONEY;
+//    }
 
     public void writeColumn(HSSFSheet sheet) {
         HSSFRow curRow;
@@ -135,16 +133,7 @@ public class Exel {
         curRow.createCell(8).setCellValue("누적 거래량");
     }
 
-    public void writeExcelFile(String fileName, String sheetName, List<CandleDTO> candleDTOS) throws EncryptedDocumentException, IOException {
-        FileInputStream fis = new FileInputStream(filePath + fileName);
-        HSSFWorkbook workbook = new HSSFWorkbook(fis);
-        HSSFSheet sheet = null;
-        try {
-            sheet = workbook.createSheet(sheetName);    // sheet 생성
-            writeColumn(sheet);
-        } catch (IllegalArgumentException e) {
-            sheet = workbook.getSheet(sheetName);
-        }
+    public void makeSheetRows(HSSFSheet sheet, List<CandleDTO> candleDTOS) throws EncryptedDocumentException, IOException {
         HSSFRow curRow;
         int lastRowNumber = sheet.getLastRowNum();
         int row = candleDTOS.size();    // list 크기
@@ -161,9 +150,22 @@ public class Exel {
             curRow.createCell(7).setCellValue(dto.getAccumulateTradePrice());
             curRow.createCell(8).setCellValue(dto.getAccumulateTradeVolume());
         }
-        fis.close();
+    }
+
+    public void writeExcelFile(String fileName, String sheetName, List<CandleDTO> candleDTOS) throws EncryptedDocumentException, IOException {
+        FileInputStream fis = new FileInputStream(filePath + fileName);
+        HSSFWorkbook workbook = new HSSFWorkbook(fis);
+        HSSFSheet sheet = null;
+        try {
+            sheet = workbook.createSheet(sheetName);    // sheet 생성
+            writeColumn(sheet);
+        } catch (IllegalArgumentException e) {
+            sheet = workbook.getSheet(sheetName);
+        }
+        makeSheetRows(sheet, candleDTOS);
         FileOutputStream fos = new FileOutputStream(filePath + fileName);
         workbook.write(fos);
+        fis.close();
         fos.close();
     }
 }
--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -1,27 +1,160 @@
 
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
 
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Simulator {
-    static String filePath = "C:\\Users\\Hojun\\Desktop\\simulation\\";
+
+    private LinkedHashMap<String, List<CandleDTO>> candleMap = new LinkedHashMap<>();
+
+    // 현재 시점, 즉 몇번째 row인지
+    private int currentRow = 0;
+    private double buyingPrice;
+    private String buyingCoin = "";
+    private double MONEY = 10_000;
+    int rowSize = 0;
+    static String filePath = "C:\\Users\\dev\\Desktop\\simulation\\";
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         Coin coin = new Coin();
         Exel exel = new Exel();
+        Simulator simulator = new Simulator();
 //        exel.makeExelFile("test.xlsx");
-//        Simulator simulator = new Simulator();
 //        simulator.makeAllCoin24hourExelData();
         FileInputStream fis = new FileInputStream(filePath + "test.xlsx");
-        double money = exel.readExelFile(fis);
-        System.out.println("money = " + money);
+        simulator.setExelDataToCandleMap(fis);
+        while (true) {
+            while (!simulator.buySomething()) {
+                simulator.findTarget();
+                System.out.println("target: " + simulator.buyingCoin);
+                if(simulator.currentRow> simulator.rowSize){
+                    break;
+                }
+//                System.out.println("currentRow: " + simulator.currentRow);
+            }
+            simulator.sellCoin();
+            if (simulator.currentRow >= simulator.rowSize) {
+                break;
+            }
+        }
+        System.out.println(simulator.MONEY);
         fis.close();
+    }
+
+    public boolean buySomething() {
+        if (buyingCoin.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    // 구매 코인을 판매함
+    public void sellCoin() {
+        if(buyingCoin.equals("")){
+            return;
+        }
+        List<CandleDTO> candleList = candleMap.get(buyingCoin);
+        List<Double> priceList = candleList.stream()
+                .map(CandleDTO::getTradePrice)
+                .toList();
+        for (int j = currentRow; j < priceList.size(); j++) {
+            double nowPrice = priceList.get(j);
+            if (nowPrice > buyingPrice * 1.015) {
+                MONEY = MONEY * 1.015;
+                buyingPrice = 0;
+                buyingCoin = "";
+                currentRow = j;
+            }
+            if (nowPrice > buyingPrice * 0.98) {
+                MONEY = MONEY * 0.98;
+                buyingPrice = 0;
+                buyingCoin = "";
+                currentRow = j;
+            }
+        }
+    }
+
+    // 모든 코인을 구매 타겟인지 확인
+    public String findTarget() {
+        Iterator<String> coinNames = candleMap.keySet().iterator();
+        while (coinNames.hasNext()) {
+            String coinName = coinNames.next();
+            if (isThisTarget(coinName)) {
+                buyingCoin = coinName;
+                return coinName;
+            }
+        }
+        return null;
+    }
+
+    // 단일 코인이 구매 타겟인지 확인
+    public boolean isThisTarget(String coinName) {
+        List<CandleDTO> candleList = candleMap.get(coinName);
+        List<Double> percentList = makeRescent10PercentList(candleList);
+        for (int i = 0; i < percentList.size() - 1; i++) {
+            if (candleList.get(0).getTradePrice() > 150 && percentList.get(i) > 0.5 && percentList.get(i + 1) > 0.5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 종가를 이용해서 최근 10분간의 상승률 퍼센트를 구한다.
+    public List<Double> makeRescent10PercentList(List<CandleDTO> candleList) {
+        List<Double> percentList = new ArrayList<>();
+        for (int j = currentRow; j < currentRow + 10; j++) {
+            if (j >= candleList.size() - 1) {
+                break;
+            }
+            double diff = candleList.get(j).getTradePrice() - candleList.get(j + 1).getTradePrice();
+            double percent = Math.round(diff / candleList.get(j + 1).getTradePrice() * 10000) / 100.0;
+            percentList.add(percent);
+        }
+        currentRow += 10;
+        return percentList;
+    }
+
+    // Exel 데이터를 필드 candelMap 으로 변환하는 함수.
+    public void setExelDataToCandleMap(FileInputStream fis) throws IOException {
+        HSSFWorkbook workbook = new HSSFWorkbook(fis);
+        int sheetNum = workbook.getNumberOfSheets();
+        for (int i = 0; i < sheetNum; i++) {
+            HSSFSheet sheet = workbook.getSheetAt(i);
+            List<CandleDTO> candleList = new ArrayList<>();
+            int rowNum = sheet.getPhysicalNumberOfRows(); // 해당 시트의 행의 개수
+            rowSize = rowNum;
+            for (int j = 1; j < rowNum; j++) {
+                HSSFRow row = sheet.getRow(j); // 각 행을 읽어온다
+                if (row == null) { // 현재 행에 아무 데이터도 없으면 빠져나옴.
+                    break;
+                }
+                CandleDTO dto = makeCandleDtoWithExelRow(row);
+                candleList.add(dto);
+            }
+            candleMap.put(sheet.getSheetName(), candleList);
+        }
+    }
+
+    // 엑셀 row를 이용해서 CandleDTO 만드는 함수
+    public CandleDTO makeCandleDtoWithExelRow(HSSFRow row) {
+        CandleDTO candleDTO = new CandleDTO();
+        candleDTO.setMarket(row.getCell(0).getStringCellValue());
+        candleDTO.setTimeUTC(row.getCell(1).getStringCellValue());
+        candleDTO.setTimeKST(row.getCell(2).getStringCellValue());
+        candleDTO.setOpeningPrince(row.getCell(3).getNumericCellValue());
+        candleDTO.setHighPrice(row.getCell(4).getNumericCellValue());
+        candleDTO.setLowPrice(row.getCell(5).getNumericCellValue());
+        candleDTO.setTradePrice(row.getCell(6).getNumericCellValue());
+        candleDTO.setAccumulateTradePrice(row.getCell(7).getStringCellValue());
+        candleDTO.setAccumulateTradeVolume(row.getCell(8).getStringCellValue());
+        return candleDTO;
     }
 
     public void makeAllCoin24hourExelData() throws IOException, NoSuchAlgorithmException {
