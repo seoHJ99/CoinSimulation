95890a434d08a78aaafd4140e7c37eb0c8480837
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-04-02 22:34:54 +0900

    Test: 추가. 분봉 데이터 날짜만큼 가져오는지 테스트

---
+++ src/test/java/CoinTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CoinTest {
+
+    private final int MINUTES_OF_DAY = 1440;
+
+
+    @Test
+    void make24dtoTest(){
+        Coin coin = new Coin();
+        int days = 3;
+        List<CandleDTO> candleDTOS = coin.make24hoursDtos("KRW-BTC", days);
+        assertThat(candleDTOS.size()).isEqualTo(MINUTES_OF_DAY * days);
+
+    }
+}
